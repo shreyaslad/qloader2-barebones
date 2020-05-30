bfa3f84397060b954af4bf0e54aa00970d8a4a49
Matteo Semenzato <mattew8898@gmail.com>
2020-05-30 11:40:04 +0200

    move stivale struct definitions to stivale.h

--- src/kernel.c
+++ src/kernel.c
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include <stivale.h>
 
 #define VGA_ADDRESS 0xb8000
 #define VGA_COLOR(character, color) (uint16_t) character | (uint16_t) color << 8
@@ -19,39 +20,6 @@
 #define VGA_YELLOW       14
 #define VGA_WHITE        15
 
-struct stivale_header {
-    uint64_t stack;
-    // Flags
-    // bit 0   0 = text mode,   1 = graphics mode
-    uint16_t flags;
-    uint16_t framebuffer_width;
-    uint16_t framebuffer_height;
-    uint16_t framebuffer_bpp;
-} __attribute__((packed));
-
-struct stivale_module {
-    uint64_t begin;
-    uint64_t end;
-    char     string[128];
-    uint64_t next;
-} __attribute__((packed));
-
-struct stivale_struct {
-    uint64_t cmdline;
-    uint64_t memory_map_addr;
-    uint64_t memory_map_entries;
-    uint64_t framebuffer_addr;
-    uint16_t framebuffer_pitch;
-    uint16_t framebuffer_width;
-    uint16_t framebuffer_height;
-    uint16_t framebuffer_bpp;
-    uint64_t rsdp;
-    uint64_t module_count;
-    uint64_t modules;
-    uint64_t epoch;
-    uint64_t flags;       // bit 0: 1 if booted with BIOS, 0 if booted with UEFI
-} __attribute__((packed));
-
 /**
  * Stack for bootstrapping the kernel
  */
@@ -70,11 +38,11 @@ struct stivale_header header = {
 uint16_t *vga_buffer;
 
 void _start(struct stivale_header *header) {
-	vga_buffer = (uint16_t*)VGA_ADDRESS;
-	vga_buffer[0] = VGA_COLOR('h', VGA_GREEN);
-	vga_buffer[1] = VGA_COLOR('e', VGA_GREEN);
-	vga_buffer[2] = VGA_COLOR('l', VGA_GREEN);
-	vga_buffer[3] = VGA_COLOR('l', VGA_GREEN);
-	vga_buffer[4] = VGA_COLOR('o', VGA_GREEN);
+    vga_buffer = (uint16_t*)VGA_ADDRESS;
+    vga_buffer[0] = VGA_COLOR('h', VGA_GREEN);
+    vga_buffer[1] = VGA_COLOR('e', VGA_GREEN);
+    vga_buffer[2] = VGA_COLOR('l', VGA_GREEN);
+    vga_buffer[3] = VGA_COLOR('l', VGA_GREEN);
+    vga_buffer[4] = VGA_COLOR('o', VGA_GREEN);
     asm volatile ("hlt");
 }
